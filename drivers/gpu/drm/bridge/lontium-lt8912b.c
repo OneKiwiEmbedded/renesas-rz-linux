f58c584fb6d48efe6e052842fbaa199a510499e9
Thanh Duong <thanh.duong.vs@gmail.com>
2025-11-04 14:39:54 +0700

    fix lontium-lt8912b

--- drivers/gpu/drm/bridge/lontium-lt8912b.c
+++ drivers/gpu/drm/bridge/lontium-lt8912b.c
@@ -48,6 +48,7 @@ struct lt8912 {
 	u8 data_lanes;
 	bool is_power_on;
 	bool is_attached;
+	bool has_connector;
 };
 
 static int lt8912_write_init_config(struct lt8912 *lt)
@@ -604,6 +605,7 @@ static int lt8912_bridge_connector_init(struct drm_bridge *bridge)
 
 	connector->dpms = DRM_MODE_DPMS_OFF;
 	drm_connector_attach_encoder(connector, bridge->encoder);
+	lt->has_connector = true;
 
 exit:
 	return ret;
@@ -647,12 +649,26 @@ static void lt8912_bridge_detach(struct drm_bridge *bridge)
 {
 	struct lt8912 *lt = bridge_to_lt8912(bridge);
 
+	/*
 	if (lt->is_attached) {
 		lt8912_detach_dsi(lt);
 		lt8912_hard_power_off(lt);
 		drm_connector_unregister(&lt->connector);
 		drm_connector_cleanup(&lt->connector);
+	}*/
+
+	if (!lt->is_attached)
+		return;
+
+	if (lt->dsi)
+		lt8912_detach_dsi(lt);
+	lt8912_hard_power_off(lt);
+
+	if (lt->has_connector) {
+		drm_connector_cleanup(&lt->connector);
+		lt->has_connector = false;
 	}
+	lt->is_attached = false;
 }
 
 static enum drm_connector_status
